5bc0c1ed97e9e126849eebd1335df0235775f2d0
Dafrisu <daffaraymisuandi@gmail.com>
2024-10-27 21:41:12 +0700

    Rename file Order jadi kelolaPesanan, nambahin 3 file buat menu tabs. pesanan diterima,ditolak,selesai

--- javascript/Dafa_Dashboard.js
+++ javascript/Dafa_Dashboard.js
@@ -2,3 +2,35 @@ function toggleSubmenu() {
     var submenu = document.getElementById("submenu");
     submenu.style.display = submenu.style.display === "block" ? "none" : "block";
 }
+
+$(document).ready(function () {
+    $("#load-pesanan").click(function () {
+        loadTasks();
+    });
+});
+
+
+function loadPesanan() {
+    $.getJSON('databarang.json', function (data) {
+        if (data.databarang && data.databarang.length > 0) {
+            displayTasks(data.tasks);
+        }
+    });
+}
+
+function displayTasks(tasks) {
+    var taskList = $("#list-Pesanan");
+    taskList.empty();
+    tasks.forEach(function (item, index) {
+        var row = `
+                    <tr>
+                        <td>${index + 1}</td>
+                        <td>${item.nama}</td>
+                        <td>-</td>
+                        <td>${item.harga}</td>
+                        <td><button class="btn btn-primary">Action</button></td>
+                    </tr>
+                `;
+        taskList.append(row);
+    });
+}

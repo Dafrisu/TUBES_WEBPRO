fe1c5d3b1495d4831a9b1c52ddfcc36c9317c0ea
Dafrisu <daffaraymisuandi@gmail.com>
2025-03-12 21:06:07 +0700

    Nambahin middleware timeout Idle

---
+++ app/Http/Middleware/IdleTimeoutMiddleware.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class IdleTimeoutMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check()) {
+            $lastActivity = Session::get('last_activity');
+            $timeout = 30; // Waktu idle dalam detik
+
+            if ($lastActivity && (time() - $lastActivity > $timeout)) {
+                Auth::logout(); // Logout user
+                Session::flush();
+                return redirect('/login')->withErrors(['message' => 'Session expired due to inactivity.']);
+            }
+
+            Session::put('last_activity', time()); // Reset timer
+        }
+
+        return $next($request);
+    }
+}

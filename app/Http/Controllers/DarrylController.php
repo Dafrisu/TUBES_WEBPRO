a547602724459fb3644895b17f27e9e8c429ffad
darryl <darrylrambi@gmail.com>
2025-04-07 17:31:49 +0700

    add auth controller, sama generate code

--- app/Http/Controllers/DarrylController.php
+++ app/Http/Controllers/DarrylController.php
@@ -97,51 +97,18 @@ class DarrylController extends Controller
     function auth(Request $request)
     {
         try {
-            $data = [
-                'inputEmail' => $request->input('inputEmail'),
-                'inputPassword' => $request->input('inputPassword'),
-            ];
-
-            Log::info('Data prepared', ['data' => $data]);
-            // pakai guzzle
-            $client = new Client(['verify' => false]);
-
-            // Send the POST request using Guzzle
-            $response = $client->post('https://umkmapi-production.up.railway.app/login', [
-                'json' => $data,
-            ]);
-
-            // check RememberMe
-            $remember = $request->has('RememberMe');
-            if ($remember) {
-                // isi cookie dengan session yang sudah disimpan kalo RememberMe
-                Cookie::queue(Cookie::make('LoginEmail', $request->input('inputEmail'), 60)); // set cookie expire sejam
-                Cookie::queue(Cookie::make('LoginPassword', $request->input('inputPassword'), 60)); // set cookie expire sejam
-            } else {
-                // Kosongkan cookie kalo tidak RememberMe
-                Cookie::expire('LoginEmail');
-                Cookie::expire('LoginPassword');
-            }
-
-            if ($response->getStatusCode() == 200) {
-                $responseData = json_decode($response->getBody()->getContents(), true);
-
-                if (isset($responseData['id_umkm'])) {
-                    session(['umkmID' => $responseData['id_umkm']]);
-
-                    return redirect()->route('umkm.dashboard')
-                        ->with('success', 'Berhasil Masuk! 👍👍');
-                } else {
-                    return redirect()->back()->with('error', 'Gagal Masuk! ID UMKM tidak ditemukan.');
-                }
-                return redirect()->route('umkm.dashboard')
-                    ->with('success', 'Berhasil Masuk! 👍👍');
-            } else {
-                return redirect()->back()
-                    ->with('error', 'Gagal Masuk! :(' . $response->getBody());
-            }
+            
         } catch (\Exception $e) {
-            return redirect()->back()->with('error', 'Gagal total pokoknya dah');
+            return redirect()->back()->with('error', 'Gagal verifikasi akun anda');
+        }
+    }
+
+    function generateCode(Request $request)
+    {
+        try {
+            
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'Gagal kirim ulang code');
         }
     }
 }

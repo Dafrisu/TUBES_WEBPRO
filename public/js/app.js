d92b562b37fd242b968159a897820da8d2bf46a2
Haikal Risnandar <haikalrisnandar82@gmail.com>
2025-06-03 18:54:16 +0700

    try ro fix catastrophe again

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,18 @@
-import './bootstrap'; // Ini biasanya mengimpor file resources/js/bootstrap.js
-import * as bootstrap from 'bootstrap'; // Mengimpor semua JS Bootstrap
+/* resources/js/app.js */
+
+import './bootstrap'; // Ini mengimpor file resources/js/bootstrap.js bawaan Laravel
+import * as bootstrap from 'bootstrap'; // Mengimpor semua JS Bootstrap jika Anda menggunakannya
+
+/* Import semua file JavaScript kustom Anda yang lain */
+import './Dafa_Dashboard.js';
+import './Dafa_Sidebar.js';
+import './darryl.js';
+import './fersya_inbox.js';
+import './Haikal_livesearch.js';
+import './Haikal_managebarang.js';
+import './Mahesa_Statistik.js';
+import './Raphael_Chat.js';
+import './Raphael_Message.js';
+
+/* Tambahkan JavaScript kustom tambahan di sini jika ada */
+console.log('Aplikasi JS berjalan!');

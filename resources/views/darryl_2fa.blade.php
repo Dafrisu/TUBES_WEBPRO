28b47a93a21af9f8d113efdfaa3ef72804b07085
darryl <darrylrambi@gmail.com>
2025-06-09 12:33:21 +0700

    quick fix kirim ulang kode otp

--- resources/views/darryl_2fa.blade.php
+++ resources/views/darryl_2fa.blade.php
@@ -92,9 +92,8 @@
                 <!-- Resend OTP -->
                 <div class="d-flex justify-content-center mt-2">
                     <div class="fs-6">Tidak menerima OTP?</div>
-                    <button type="submit" name="action" value="kirim-ulang" class="btn btn-link p-0 align-baseline">
-                        Kirim ulang
-                    </button>
+                    <a class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover ms-1"
+                        href="{{ route('umkm.kirim-code') }}">Kirim ulang</a>
                 </div>
             </form>
         </div>
